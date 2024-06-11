75186d02662e6af1046fb3bc48f4a0ff66dd6c39
eLDoherty <leonardodoherty@gmail.com>
2024-06-11 20:13:07 +0700

    New Update

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// Get the height of the navbar
-const navbarHeight = document.querySelector('nav').offsetHeight;
-// Add an event listener to each navbar link
-document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
 
-    // Get the ID of the section to scroll to
-    const sectionId = this.getAttribute('href');
-
-    // Get the top position of the section
-    const sectionTop = document.querySelector(sectionId).offsetTop;
-
-    // Scroll to the section, accounting for the navbar height
-    window.scrollTo({
-      top: sectionTop - navbarHeight,
-      behavior: 'smooth'
-    });
-  });
-});
 
 document.addEventListener('DOMContentLoaded', function() {
+
+  // Header
+  const navbarHeight = document.querySelector('nav').offsetHeight;
+
+  document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const sectionId = this.getAttribute('href');
+      const sectionTop = document.querySelector(sectionId).offsetTop;
+
+      window.scrollTo({
+        top: sectionTop - navbarHeight,
+        behavior: 'smooth'
+      });
+    });
+  });
+
     const sections = document.querySelectorAll('section');
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
@@ -32,4 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
     sections.forEach(section => {
         observer.observe(section);
     });
+
+    // Aos init
+    AOS.init();
 });
